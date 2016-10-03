40d7af5d51f3a1004e5748f631c1013911e8e34f
jessmccay <jessmccay@gmail.com>
2016-10-02 22:22:35 -0700

    Add files via upload

---
+++ the_dom.js
@@ -0,0 +1,13 @@
+
+
+function makeCar() {
+  var value =
+   document.getElementById("userInput").value;
+   document.getElementById("output").innerHTML = value;
+   var value2 =
+    document.getElementById("userInput2").value;
+    document.getElementById("output2").innerHTML = value2;
+    var value3 =
+    document.getElementById("userInput3").value;
+   document.getElementById("output3").innerHTML = value3;
+};
